c2f9af60b2325b3e6af9fefb8c09edf320e9f11c
Alex Reynolds <cam00674@myport.ac.uk>
2014-03-13 14:41:36 +0000

    Basic ui styles and adding nodes to map

---
+++ ui.js
@@ -0,0 +1,34 @@
+$(function(){
+	$('#node').bind('click',function(){
+		$('#mycanvas').unbind('click').bind('click',function(e){
+			coords = relMouseCoords(this,e);
+			geo.nodes.E = new geo.node('E',coords);
+		})
+	});
+	$('#hazard').bind('click',function(){
+		alert('hazard');
+	});
+});
+
+/*
+	http://stackoverflow.com/questions/55677/how-do-i-get-the-coordinates-of-a-mouse-click-on-a-canvas-element
+*/
+function relMouseCoords(canvas,event){
+    var totalOffsetX = 0;
+    var totalOffsetY = 0;
+    var canvasX = 0;
+    var canvasY = 0;
+    var currentElement = canvas;
+    var height = currentElement.height
+
+    do{
+        totalOffsetX += currentElement.offsetLeft - currentElement.scrollLeft;
+        totalOffsetY += currentElement.offsetTop - currentElement.scrollTop;
+    }
+    while(currentElement = currentElement.offsetParent)
+
+    canvasX = event.pageX - totalOffsetX;
+    canvasY = height - ( event.pageY - totalOffsetY );
+
+    return {x:canvasX, y:canvasY}
+}
