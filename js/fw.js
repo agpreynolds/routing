1c21b1fd70a288a6fc045323605bc7a3839aff0f
benjanyan <ben@bmcq.co.uk>
2014-03-20 14:07:15 +0000

    FW structue added

--- js/fw.js
+++ js/fw.js
@@ -1,7 +1,18 @@
 //TODO: Implement Floyd-Warshall
 
 $(function() {
-	var floydWarshall = function(startNode, targetNode) {
+	var floydWarshall = function(nodes) {
+		//TODO: Create matrix of paths and their weights
+		var distances = [];
 		
-	}
-}
+		for (var i in nodes) {
+			for (var j in nodes) {
+				
+			}
+		}
+		
+		var getBestRoute = function(startNode, endNode) {
+			//TODO: Return best route
+		};
+	};
+});
